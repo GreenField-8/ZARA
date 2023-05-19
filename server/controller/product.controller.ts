1800953d45551a20470ccd0153e21279453e8f08
Nacef19 <117386346+Nacef19@users.noreply.github.com>
2023-05-19 11:28:10 +0100

    get by category done

---
+++ server/controller/product.controller.ts
@@ -0,0 +1,52 @@
+import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+import { Product } from "../model/product.model";
+
+
+export const createProduct = async (req: Request, res: Response) => {
+    const { name, image, category, price, status } = req.body;
+
+    const type = await getRepository(Product).insert({
+        name,
+        image,
+        category,
+        price,
+        status
+    })
+
+    res.send(type);
+}
+
+export const getAllProducts = async (req: Request, res: Response) => {
+    const products = await getRepository(Product).find();
+
+    res.send(products);
+};
+
+export const getProductById = async (req: Request, res: Response) => {
+    try {
+    const product = await getRepository(Product).findOne({where: {id: parseInt(req.params.id, 10)}});
+
+    if (product) {
+        res.send(product);
+    } else {
+        res.status(404).send('Product not found');
+    }
+    } catch (error) {
+    res.status(500).send('Error retrieving product');
+    }
+};
+
+export const getProductByCategory = async (req: Request, res: Response) => {
+    try {
+        const product = await getRepository(Product).findOne({where: {category: parseInt(req.params.category, 10)}});
+    
+        if (product) {
+            res.send(product);
+        } else {
+            res.status(404).send('Product not found');
+        }
+        } catch (error) {
+        res.status(500).send('Error retrieving product');
+        }
+};
