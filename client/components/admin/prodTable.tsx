056645493febc03ca6f328ceb1addcc12a37f623
^[[200~Islem-Az <isleem.azouuz@gmail.com>
2023-05-20 11:32:28 +0200

    create admin interface and styled it , everything is working on it

---
+++ client/components/admin/prodTable.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import styles from './prodmange.module.css';
+
+interface Product {
+  id: number;
+  name: string;
+  image: string;
+  category: string;
+  price: number;
+  status: string;
+}
+
+interface ProductTableProps {
+  products: Product[];
+  onEdit: (product: Product) => void;
+  onDelete: (productId: number) => void;
+}
+
+const ProductTable: React.FC<ProductTableProps> = ({ products, onEdit, onDelete }) => {
+  return (
+    <div className={styles.container}>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Image</th>
+            <th>Category</th>
+            <th>Price</th>
+            <th>Status</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product) => (
+            <tr key={product.id}>
+              <td>{product.name}</td>
+              <td>{product.image}</td>
+              <td>{product.category}</td>
+              <td>{product.price}</td>
+              <td>{product.status}</td>
+              <td>
+                <button className={`${styles.edit}`} onClick={() => onEdit(product)}>
+                  Edit
+                </button>
+              </td>
+              <td>
+                <button className={`${styles.delete}`} onClick={() => onDelete(product.id)}>
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default ProductTable;
