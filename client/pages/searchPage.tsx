697ca2fce06e4369413bd15d4d4c1d6ad7196ff9
^[[200~Islem-Az <isleem.azouuz@gmail.com>
2023-05-21 16:15:06 +0200

    search results styled and done

--- client/pages/searchPage.tsx
+++ client/pages/searchPage.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Navbar from '@/components/navbar/navbar';
-import Link from 'next/link';
 
 const SearchPage = () => {
   const [searchQuery, setSearchQuery] = useState('');
@@ -48,48 +47,58 @@ const SearchPage = () => {
           onChange={(e) => setSearchQuery(e.target.value)}
         />
       </div>
-      <div>
+      <div className="product-list">
         {searchResults.map((product) => (
           <div key={product.id} className="product-container">
-            <Link href={`/postDetailsPage?id=${product.id}&name=${encodeURIComponent(product.name)}&price=${product.price}&image=${encodeURIComponent(product.image)}&description=${encodeURIComponent(product.description)}`}>
-              <div>
-                <img
-                  src={product.image}
-                  alt="Product Image"
-                  style={{ width: '232px', height: '348px', aspectRatio: '2/3' }}
-                />
-                <p className="product-info">
-                  <span className="product-name">{product.name}</span>
-                  <span className="product-price">{product.price} TND</span>
-                </p>
+            <div
+              onClick={() =>
+                window.location.href = `/postDetailsPage?id=${product.id}&name=${encodeURIComponent(product.name)}&price=${product.price}&image=${encodeURIComponent(product.image)}&description=${encodeURIComponent(product.description)}`
+              }
+            >
+              <img
+                src={product.image}
+                alt="Product Image"
+                style={{ width: '232px', height: '348px', aspectRatio: '2/3' }}
+              />
+              <div className="product-details">
+                <p className="product-name">{product.name}</p>
+                <p className="product-price">{product.price} TND</p>
               </div>
-            </Link>
+            </div>
           </div>
         ))}
       </div>
 
       <style jsx>{`
-        .product-container {
-          flex-direction: column;
-          align-items: center;
+        .product-list {
+          margin-top: 20px; /* Adjust the margin value as needed */
         }
 
-        .product-info {
+        .product-container {
           display: flex;
+          flex-direction: column;
           align-items: center;
+          margin-bottom: 20px; /* Adjust the margin value as needed */
+        }
+
+        .product-details {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          margin-top: 5px;
           width: 100%;
-          margin-bottom: 10px; /* Adjust the margin value as needed */
         }
 
         .product-name {
           font-family: 'Neue Helvetica', Arial, sans-serif;
           font-size: 10px;
-          margin-right: 150px;
+          margin-top: 5px;
         }
 
         .product-price {
           font-family: 'Neue Helvetica', Arial, sans-serif;
           font-size: 10px;
+          margin-top: 5px;
         }
       `}</style>
     </div>
